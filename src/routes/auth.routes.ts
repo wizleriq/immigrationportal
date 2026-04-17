cd7d184601224c6be117a21e31e2d0794c059be4
Ndata <wisdomndata@gmail.com>
2026-04-17 09:46:04 +0100

    Backend

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { register, login } from "../services/auth/auth.controller";
+import { register, login, createAgent } from "../services/auth/auth.controller";
+import authMiddleware from "../middleware/auth.middleware";
 // import { register, login } from "../services/auth/auth.controller"; 
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/become-agent", authMiddleware, createAgent); 
 
 // // your routes here
 // router.post("/login", ...);
